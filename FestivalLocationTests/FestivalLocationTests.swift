1fc31e24e6f72e751780dca2341c5ba39c855967
cerihughes <github@cerihughes.co.uk>
2023-07-31 14:50:05 +0100

    Update FestivalLocationTests.swift

--- FestivalLocationTests/FestivalLocationTests.swift
+++ FestivalLocationTests/FestivalLocationTests.swift
@@ -1,10 +1,3 @@
-//
-//  FestivalLocationTests.swift
-//  FestivalLocationTests
-//
-//  Created by Ceri Hughes on 31/07/2023.
-//
-
 import XCTest
 
 final class FestivalLocationTests: XCTestCase {}
