04b380b9450559acb338b36bf9f8a079811321bc
cerihughes <github@cerihughes.co.uk>
2023-08-13 13:55:40 +0100

    Add initial tests

---
+++ FestivalLocationTests/UI/FestivalData/FestivalDataViewModelTests.swift
@@ -0,0 +1,38 @@
+import RealmSwift
+import XCTest
+
+@testable import FestivalLocation
+
+final class FestivalDataViewModelTests: XCTestCase {
+    private var dataRepository: DataRepository!
+    private var dataLoader: FileDataLoader!
+    private var viewModel: FestivalDataViewModel!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        let config = Realm.Configuration(inMemoryIdentifier: "FestivalDataViewModelTests")
+        let realm = try Realm(configuration: config)
+        dataRepository = RealmDataRepository(realm: realm)
+        dataLoader = FileDataLoader(fileName: "GreenMan2023.txt", dataRepository: dataRepository)
+        viewModel = .init(dataRepository: dataRepository, dataLoader: dataLoader)
+    }
+
+    override func tearDownWithError() throws {
+        dataRepository = nil
+        dataLoader = nil
+        viewModel = nil
+        try super.tearDownWithError()
+    }
+
+    func testInitialData() {
+        XCTAssertEqual(viewModel.numberOfSlots, 0)
+    }
+
+    func testSundayTwist() {
+        viewModel.selectedDay = .sunday
+        viewModel.selectedStage = .roundTheTwist
+
+        XCTAssertEqual(viewModel.numberOfSlots, 6)
+    }
+}
